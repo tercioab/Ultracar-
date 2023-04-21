989a1a002cba89adc16f8893d109ae37fb126ff7
tercioab <suaagenciainicial@gmail.com>
2023-04-21 19:28:19 -0300

    refactore: size qr code

--- ultracar/src/pages/RegisterClient.js
+++ ultracar/src/pages/RegisterClient.js
@@ -42,76 +42,74 @@ export default function RegisterClient() {
 
 	return (
 		<div className='container mx-auto px-4 py-8'>
-			<div className='flex flex-col items-center justify-center'>
-				<h1 className='text-3xl font-bold mb-4'>Novo Cliente</h1>
-				<div className='w-full md:w-1/2 lg:w-1/3'>
-					<form onSubmit={handleFormSubmit}>
-						<div className='mb-4'>
-							<label htmlFor='name' className='block font-bold text-gray-700 mb-2'>
-								Nome:
-							</label>
-							<input
-								type='text'
-								id='name'
-								value={name}
-								onChange={e => setName(e.target.value)}
-								className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-								required
-							/>
-						</div>
-
-						<div className='mb-4'>
-							<label htmlFor='plate' className='block font-bold text-gray-700 mb-2'>
-								Placa:
-							</label>
-							<input
-								type='text'
-								id='plate'
-								value={plate}
-								onChange={e => setPlate(e.target.value)}
-								className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-								required
-							/>
-						</div>
-
-						<div className='mb-4'>
-							<label htmlFor='model' className='block font-bold text-gray-700 mb-2'>
-								Modelo:
-							</label>
-							<input
-								type='text'
-								id='model'
-								value={model}
-								onChange={e => setModel(e.target.value)}
-								className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
-								required
-							/>
-						</div>
-
-						<button
-							type='submit'
-							className='bg-blue-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full'
-						>
-							Registrar Cliente
-						</button>
-					</form>
-					<button
-						className='bg-green hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-2 w-full'
-						onClick={handleDownloadQRCode}
-					>
-						Download qr code
-					</button>
-				</div>
-				{qrCodeData && (
-					<div className='mt-8 w-full md:w-1/2 lg:w-1/3'>
-						<div ref={qrCodeRef} className='flex flex-col items-center'>
-							<QRCode value={qrCodeData} size={256} id='qr-code' className='sm:w-48 md:w-64 lg:w-96 xl:w-128' />
-								<span className='text-lg font-bold mb-2'>Placa: {plate}</span>
-								<span className='text-lg font-bold mb-2'>Modelo: {model}</span>
-						</div>
-					</div>
-				)}
+		<div className='flex flex-col items-center justify-center'>
+		  <h1 className='text-3xl font-bold mb-4'>Novo Cliente</h1>
+		  <div className='w-full md:w-1/2 lg:w-1/3'>
+			<form onSubmit={handleFormSubmit}>
+			  <div className='mb-4'>
+				<label htmlFor='name' className='block font-bold text-gray-700 mb-2'>
+				  Nome:
+				</label>
+				<input
+				  type='text'
+				  id='name'
+				  value={name}
+				  onChange={e => setName(e.target.value)}
+				  className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+				  required
+				/>
+			  </div>
+	  
+			  <div className='mb-4'>
+				<label htmlFor='plate' className='block font-bold text-gray-700 mb-2'>
+				  Placa:
+				</label>
+				<input
+				  type='text'
+				  id='plate'
+				  value={plate}
+				  onChange={e => setPlate(e.target.value)}
+				  className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+				  required
+				/>
+			  </div>
+	  
+			  <div className='mb-4'>
+				<label htmlFor='model' className='block font-bold text-gray-700 mb-2'>
+				  Modelo:
+				</label>
+				<input
+				  type='text'
+				  id='model'
+				  value={model}
+				  onChange={e => setModel(e.target.value)}
+				  className='appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+				  required
+				/>
+			  </div>
+	  
+			  <button
+				type='submit'
+				className='bg-blue-2 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline w-full'
+			  >
+				Registrar Cliente
+			  </button>
+			</form>
+			<button
+			  className='bg-green hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mt-2 w-full'
+			  onClick={handleDownloadQRCode}
+			>
+			  Download qr code
+			</button>
+		  </div>
+		  {qrCodeData && (
+			<div className='mt-8 w-full md:w-1/2 lg:w-1/3'>
+			  <div ref={qrCodeRef} className='flex justify-center'>
+							<QRCode value={qrCodeData} size={256} id='qr-code' />
+			  </div>
 			</div>
+		  )}
 		</div>
+	  </div>
 	);
 }
